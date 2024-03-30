a385dd3750c3e3482fb71209fe5c93a803cae7fb
willokans <Slide2014!>
2024-03-30 11:34:32 +0000

    Add room model, setup serialization and room controller endpoint

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from rest_framework import generics
+from .models import Room
+from .serializers import RoomSerializer
 
 # Create your views here.
 
 
-def main(request):
-    return HttpResponse("<h1>Hello!</h1>")
+# Write an API view to let use view a list of all of the different room
+class RoomView(generics.ListAPIView):
+    queryset = Room.objects.all()
+    serializer_class = RoomSerializer
